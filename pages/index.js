17b9c001aa734c5597db855a66d8b004854b164a
Satyanarayana Devi <connect2satyam@gmail.com>
2022-03-14 08:26:03 +0530

    footer added

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,13 @@
 import Navbar from "../components/Navbar.tsx";
 import Sidebar from "../components/Sidebar";
+import LargeWithLogoCentered from "../components/Footer.tsx";
 
 export default function Home() {
   return (
     <>
       <Navbar />
       <Sidebar />
+      <LargeWithLogoCentered />
     </>
   );
 }
